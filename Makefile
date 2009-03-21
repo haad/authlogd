b742551e20df988bf5d6ac910fe143e3626aa241
Adam Hamsik <haad@netbsd.org>
2009-03-21 09:17:07 +0100

    Add message parsing routines.

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ USE_FORT?= yes	# network server
 WARN = 4
 
 PROG=	authlogd
-SRCS=	authlogd.c auth_mod.c auth_mod_hash.c auth_mod_gid.c config.c
+SRCS=	authlogd.c auth_mod.c auth_mod_hash.c auth_mod_gid.c config.c msg.c
 MKMAN= no
 CPPFLAGS+= -DAUTHLOGD_DEBUG
 CFLAGS += -g
---
+++ msg.c
@@ -0,0 +1,121 @@
+#include <sys/param.h>
+#include <sys/types.h>
+
+#include <err.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "authlogd.h"
+
+
+static void find_sd(msg_t *);
+static void get_auth_sd(msg_t *);
+
+/*!
+ * Parse syslog message and fills msg_t structure.
+ */
+void
+parse_msg(msg_t *msg)
+{
+	find_sd(msg);
+	get_auth_sd(msg);
+
+	printf("%s%s%s\n", msg->msg_header, msg->msg_auth_sd, msg->msg_body);
+}
+
+
+/*!
+ * Create and sign Authlog message SD element. This element will be later
+ * appended to original syslog message.
+ * @param msg General syslog message description structure.
+ */
+static void
+get_auth_sd(msg_t *msg)
+{
+	char *sign = '\0';
+	char *status;
+	
+#define AUTHORIZE   "Authorized"
+#define DENY        "Denied"
+#define UNKNOWN     "Unknown"	
+	switch (msg->auth_msg->msg_auth_status) {
+	case 0:
+		status = DENY;
+		break;
+	case 1:
+		status = AUTHORIZE;
+		break;
+	case 2:
+		status = UNKNOWN;
+		break;
+	}
+		
+	snprintf(msg->msg_auth_sd, AUTHLOG_AUTH_SD, " [@authlogd msg=\"%s\" sign=\"%s\"]", status, sign);
+		
+	return;
+}
+
+/*!
+ * Parse syslog message and find place where we can place Authlogd SD element.
+ * There are 2 possible cases:
+ * 1) There is no SD element in present message then we are looking for '-'
+ *    after 6th ' ' char.
+ * 2) There is already SD element present in message we will add auth sd element
+ *    before it.
+ * In both cases I set msg::msg_header to string which ends in place of 6th space
+ * with '\0'. msg::msg_body is set to other part of message wich starts with
+ * '[' in case the second or with ' ' in the first case.
+ * @param msg General message structure.
+ */
+static void
+find_sd(msg_t *msg)
+{
+	int space;
+	char *str;
+	int i;
+
+	str = msg->msg_buf;
+	space = 0;
+	
+	for (str = msg->msg_buf; str != '\0'; str++) {
+		
+		if (*str == ' ') {
+			space++;
+//			printf("Found space in a message number is %d\n", space);
+		} else
+			continue;
+
+		if (space == 6) {
+//			printf("Found sd element part of message\n");
+			str++;
+
+//			printf("|%c|++|%s|\n", *str, str);
+
+			/** There are no SD elements in message */
+			if (*str == '-') {
+				msg->msg_header = msg->msg_buf;
+				msg->msg_body = ++str;
+
+				str -= 2;
+				*str = '\0';
+			}
+
+			if (*str == '[') {
+				msg->msg_body = str;
+				msg->msg_header = msg->msg_buf;
+								
+				str--;
+				*str = '\0';
+			}
+			
+//			printf("msg->msg_header |%s|\n", msg->msg_header);
+//			printf("msg->msg_body |%s|\n", msg->msg_body);
+			
+			break;
+		}
+	}
+	return;
+}
